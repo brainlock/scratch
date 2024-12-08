6a6baf43826462eb538d9ecd403b701c52c5dd8c
Alberto Piai <apiai@sensational.ch>
2024-12-08 23:25:03 +0100

    fix inefficient append

--- aoc2024/src/Day03.hs
+++ aoc2024/src/Day03.hs
@@ -15,9 +15,20 @@ data State =
     | CtMul2 Int Int
     deriving (Show)
 
-type S = ([Exp], State, String)
+newtype Prg = Prg [Exp]
 
-tryParseInt :: [Exp] -> String -> (Int -> State) -> S
+appendExp :: Prg -> Exp -> Prg
+appendExp (Prg exps) e = Prg $ e:exps
+
+emptyPrg :: Prg
+emptyPrg = Prg []
+
+getExps :: Prg -> [Exp]
+getExps (Prg exps)= reverse exps
+
+type S = (Prg, State, String)
+
+tryParseInt :: Prg -> String -> (Int -> State) -> S
 tryParseInt p str constr = case reads str of
     [(n, rest)] -> (p, constr n, rest)
     [] -> (p, Init, str)
@@ -25,20 +36,20 @@ tryParseInt p str constr = case reads str of
 
 parseStep :: S -> S
 parseStep (p, s, "") = (p, s, "")
-parseStep (p, Init, 'd':'o':'(':')':rest) = (p ++ [Do], Init, rest)
-parseStep (p, Init, 'd':'o':'n':'\'':'t':'(':')':rest) = (p ++ [Dont], Init, rest)
+parseStep (p, Init, 'd':'o':'(':')':rest) = (appendExp p Do, Init, rest)
+parseStep (p, Init, 'd':'o':'n':'\'':'t':'(':')':rest) = (appendExp p Dont, Init, rest)
 parseStep (p, Init, 'm':'u':'l':'(':rest) = (p, CtMul, rest)
 parseStep (p, CtMul, s) = tryParseInt p s CtMul1
 parseStep (p, CtMul1 arg1, ',':s) = tryParseInt p s (CtMul2 arg1)
-parseStep (p, CtMul2 arg1 arg2, ')':s) = (p ++ [Mul arg1 arg2], Init, s)
+parseStep (p, CtMul2 arg1 arg2, ')':s) = (appendExp p (Mul arg1 arg2), Init, s)
 parseStep (p, _, _:rest) = (p, Init, rest)
 
 trace :: (S, [S]) -> (S, [S])
 trace (state, t) = case parseStep state of
     (p, s, "") -> ((p, s, ""), t)
-    next -> trace (next, t ++ [next])
+    next -> trace (next, next:t)
 
-parse :: String -> [Exp]
+parse :: String -> Prg
 parse str = parse' $ inject str
     where
         parse' state = case parseStep state of
@@ -52,19 +63,19 @@ testInput2 :: String
 testInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 inject :: String -> S
-inject s = ([], Init, s)
+inject s = (emptyPrg, Init, s)
 
 eval1 :: Exp -> Maybe Int
 eval1 (Mul a b) = Just $ a * b
 eval1 _ = Nothing
 
-solve1 :: [Exp] -> Int
-solve1 muls = mapMaybe eval1 muls & sum
+solve1 :: Prg -> Int
+solve1 muls = mapMaybe eval1 (getExps muls) & sum
 
 data MulState = Enabled | Disabled
 
-solve2 :: [Exp] -> Int
-solve2 prog = foldl' eval2 (Enabled, 0) prog & snd
+solve2 :: Prg -> Int
+solve2 prog = foldl' eval2 (Enabled, 0) (getExps prog) & snd
     where
         eval2 (_, total) Do = (Enabled, total)
         eval2 (_, total) Dont = (Disabled, total)
