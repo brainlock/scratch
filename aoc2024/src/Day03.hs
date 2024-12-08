831f597fe814e1f90573c77d79fd37c605934d05
Alberto Piai <apiai@sensational.ch>
2024-12-08 11:11:19 +0100

    aoc 2024, day 3 part 2

--- aoc2024/src/Day03.hs
+++ aoc2024/src/Day03.hs
@@ -1,7 +1,11 @@
 module Main where
 import Data.Function ((&))
+import Data.Maybe (mapMaybe)
+import Data.Foldable (Foldable(foldl'))
 
-data Exp = Mul Int Int | Foo
+data Exp = Mul Int Int
+    | Do
+    | Dont
     deriving (Show)
 
 data State =
@@ -15,6 +19,8 @@ type S = ([Exp], State, String)
 
 parseStep :: S -> S
 parseStep (p, s, "") = (p, s, "")
+parseStep (p, Init, 'd':'o':'(':')':rest) = (p ++ [Do], Init, rest)
+parseStep (p, Init, 'd':'o':'n':'\'':'t':'(':')':rest) = (p ++ [Dont], Init, rest)
 parseStep (p, Init, 'm':'u':'l':'(':rest) = (p, CtMul, rest)
 parseStep (p, CtMul, s) = case reads s of
     [(n, rest)] -> (p, CtMul1 n, rest)
@@ -40,15 +46,28 @@ parse str = parse' $ inject str
 testInput :: String
 testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
+testInput2 :: String
+testInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
 inject :: String -> S
 inject s = ([], Init, s)
 
-eval :: Exp -> Int
-eval (Mul a b) = a * b
-eval _ = undefined
+eval1 :: Exp -> Maybe Int
+eval1 (Mul a b) = Just $ a * b
+eval1 _ = Nothing
 
 solve1 :: [Exp] -> Int
-solve1 muls = map eval muls & sum
+solve1 muls = mapMaybe eval1 muls & sum
+
+data MulState = Enabled | Disabled
+
+solve2 :: [Exp] -> Int
+solve2 prog = foldl' eval2 (Enabled, 0) prog & snd
+    where
+        eval2 (_, total) Do = (Enabled, total)
+        eval2 (_, total) Dont = (Disabled, total)
+        eval2 (Enabled, total) (Mul a b) = (Enabled, total + (a * b))
+        eval2 (Disabled, total) (Mul _ _) = (Disabled, total)
 
 main :: IO ()
 main = do
@@ -58,3 +77,9 @@ main = do
     input <- readFile "inputs/day03"
     let result1 = parse input & solve1
     putStrLn $ "result1: " ++ show result1
+
+    let test2 = parse testInput2 & solve2
+    print test2
+
+    let result2 = parse input & solve2
+    putStrLn $ "result2: " ++ show result2
