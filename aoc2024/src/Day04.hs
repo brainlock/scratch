e418dcabcb6cbc27871dd06944cc1c2d2a9a61bd
Alberto Piai <alberto.piai@gmail.com>
2024-12-10 17:33:23 +0100

    aoc 2024, day 4 part 2

--- aoc2024/src/Day04.hs
+++ aoc2024/src/Day04.hs
@@ -39,11 +39,11 @@ idxAdd a b = (fst a + fst b, snd a + snd b)
 
 genSeqs :: Input -> [[Idx]]
 genSeqs arr = concat
-    [genWithMove step arr | step <-
+    [genWithMove 4 step arr | step <-
         [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]]
 
-genWithMove :: Idx -> Input -> [[Idx]]
-genWithMove step arr = map (take 4 . seqFromIdx) (A.indices arr) & filter (\x -> length x == 4)
+genWithMove :: Int -> Idx -> Input -> [[Idx]]
+genWithMove n step arr = map (take n . seqFromIdx) (A.indices arr) & filter (\x -> length x == n)
     where
         seqFromIdx :: Idx -> [Idx]
         seqFromIdx idx = if inRange arr idx then idx:seqFromIdx (idxAdd idx step) else []
@@ -54,9 +54,22 @@ getStr arr = map (arr A.!)
 solve1 :: Input -> Int
 solve1 arr = genSeqs arr & map (getStr arr) & filter ("XMAS" ==) & length
 
+solve2 :: Input -> Int
+solve2 arr = A.elems counts & filter (== 2) & length
+    where
+        diags = concatMap findMas [genWithMove 3 step arr | step <- [(1, 1), (-1, -1), (-1, 1), (1, -1)]]
+        findMas = filter (\idxs -> "MAS" == map (arr A.!) idxs)
+        counts = A.accumArray (+) 0 (A.bounds arr) (concatMap getScore diags)
+        getScore :: [Idx] -> [(Idx, Int)]
+        getScore idxs = enumerate idxs & map (\(pos, idx) -> (idx, score pos))
+        enumerate = zip ([0..]::[Int])
+        score pos = if pos == 1 then 1 else 0
+
 main :: IO ()
 main = do
     let t = parse testInput
     putStrLn $ "test input 1: " ++ show (solve1 t)
     input <- readFile "inputs/day04" <&> parse
     putStrLn $ "solution 1: " ++ show (solve1 input)
+    putStrLn $ "test input 2: " ++ show (solve2 t)
+    putStrLn $ "solution 2: " ++ show (solve2 input)
