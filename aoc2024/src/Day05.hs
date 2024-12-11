344152df6ac0a5b63555b78814533c77ef4dd03b
Alberto Piai <alberto.piai@gmail.com>
2024-12-11 18:22:37 +0100

    aoc 2024, day 5 part 1

---
+++ aoc2024/src/Day05.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main where
+
+import Data.Maybe
+import Text.RawString.QQ (r)
+import Data.Function ((&))
+import qualified Data.Map as M
+import Data.Map (Map)
+import Data.List.Split (splitOn)
+import Data.Foldable (Foldable(foldl'))
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Functor ((<&>))
+
+testInput :: String
+testInput = [r|47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47|]
+
+type PageN = Int
+type PrintJob = [PageN]
+type Dependencies = Map PageN (Set PageN)
+
+data Input = Input {
+    dependencies :: Dependencies,
+    printJobs :: [PrintJob]
+} deriving (Show)
+
+parse :: String -> Input
+parse inputString = Input { dependencies=d, printJobs=pjs }
+    where
+        d = parseDeps (head parts)
+        pjs = parseJobs (parts !! 1)
+        parts = splitOn "\n\n" inputString
+
+parseDeps :: String -> Dependencies
+parseDeps s = lines s
+    & map readDepLine
+    & M.fromListWith S.union
+
+readDepLine :: String -> (Int, Set Int)
+readDepLine s = splitOn "|" s & \parts -> (read $ parts !! 1, S.singleton (read $ head parts))
+
+parseJobs :: String -> [PrintJob]
+parseJobs s = lines s & map (map read . splitOn ",")
+
+isCorrectOrder :: Input -> PrintJob -> Bool
+isCorrectOrder input pages = foldl' f (True, S.empty) pages & fst
+    where
+        deps = subGraph (dependencies input) (S.fromList pages)
+        subGraph depsMap problemPages = depsMap
+            & M.filterWithKey (\k _ -> k `S.member` problemPages)
+            & M.map (S.filter (`S.member` problemPages))
+        f (isCorrect, seen) page = (isCorrect && verifyPage page (S.insert page seen), S.insert page seen)
+        verifyPage page seen = all (`S.member` seen) (depsOfPage page)
+        depsOfPage page = Data.Maybe.fromMaybe S.empty $ M.lookup page deps
+
+solve1 :: Input -> Int
+solve1 input = sum $ map middle correctJobs
+    where
+        correctJobs = filter (isCorrectOrder input) (printJobs input)
+        middle xs = xs !! (length xs `div` 2)
+
+test :: String -> Bool -> IO ()
+test name f = do
+    putStr $ if f then "✅ " else "❌ "
+    putStrLn name
+
+main :: IO ()
+main = do
+    --putStrLn $ "deps: " ++ show (dependencies t)
+    --putStrLn $ "jobs: " ++ show (printJobs t)
+    --let expected = [True, True, True, False, False, False]
+    --forM_ (zip (printJobs t) expected) $ \(job, result) -> do
+        --test (show job) $ result == isCorrectOrder t job
+
+    let t = testInput & parse
+    putStrLn $ "test input 1: " ++ show (solve1 t)
+    input <- readFile "inputs/day05" <&> parse
+    putStrLn $ "solution 1: " ++ show (solve1 input)
